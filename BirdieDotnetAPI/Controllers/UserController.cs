f15af5be7ed2adf70428f7fbedaf64d258b64a96
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2023-05-24 19:47:21 +0200

    Implementing /user route

---
+++ BirdieDotnetAPI/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace BirdieDotnetAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            Console.WriteLine("Received GET at /user");
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+        
+            string RequestBody = HttpContext.Request.Body.ToString();
+
+            Console.WriteLine(RequestBody);
+
+            return Ok();
+        }
+
+    }
+}
